0bef5d79d3da1d0ea1382b14f2b948b048fd5126
Jose Hbez <jose.hbez@gmail.com>
2021-05-15 11:17:00 -0500

    add arg -s/--storage custom data storage

--- cmd/main.py
+++ cmd/main.py
@@ -23,7 +23,7 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 from web.main import app as webapp
-from models.base import setup, db
+from models.base import setup as setup_db, db
 from models.time_entry import TimeEntry
 from models.project import Project
 from models.project_task import ProjectTask
@@ -33,25 +33,58 @@ from models.objective_keyresult_checkin import ObjectiveKeyResultCheckin
 from models.habit import Habit
 from models.habit_checkin import HabitCheckin
 
+def extract_path(args):
+    path = click.get_app_dir('tona')
+
+    def index_path(key_arg):
+        index = -1
+        for i, arg in enumerate(args):
+            if key_arg == arg:
+                index = i + 1
+                break
+        return args[index]
+
+    if '-s' in args:
+        path = index_path('-s')
+    elif '--storage' in args:
+        path = index_path('--storage')
+    elif os.environ.get('TONA_STORAGE', False):
+        path = os.environ.get('TONA_STORAGE')
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+    return path
+
 @click.group()
-def cli():
-    setup("tona.db")
+@click.pass_context
+def cli(ctx):
+    path = extract_path(ctx.obj)
+    webapp.config['STORAGE'] = path
+    setup_db(os.path.join(path, "tona.db"))
     db.create_tables([TimeEntry,
                         Project, ProjectTask,
                         Objective, ObjectiveKeyResult, ObjectiveKeyResultCheckin,
                         Habit, HabitCheckin])
 
+
+help_storage = "Custom data storage e.g ~/tona-data or skip this option exporing var e.g TONA_STORAGE=~/tona-data"
+help_time_zone = "Frontend render datetime e.g America/Mexico_City"
+
 @click.command(name="webapp")
 @click.option("--debug", "-d", is_flag=True)
 @click.option("--port", "-p", type=click.INT, default=5001)
-@click.option("--time-zone", "-t", type=click.STRING, default="UTC", help="Linux tz: https://superuser.com/a/1589527")
-def cli_webapp(time_zone, port, debug):
+@click.option("--time-zone", "-t", type=click.STRING, default="UTC", help=help_time_zone)
+@click.option("--storage", "-s", type=click.STRING, help=help_storage)
+def cli_webapp(time_zone, port, debug, storage):
+
     webapp.secret_key = os.urandom(16)
     webapp.config['TZ'] = time_zone
+    webapp.logger.info("Time Zone: ", time_zone)
+    webapp.logger.info("Storage: ", storage)
     webapp.run(debug=debug, host='0.0.0.0', port=port)
 
 
 cli.add_command(cli_webapp)
 
 if __name__ == "__main__":
-    cli()
+    cli(obj=sys.argv)
